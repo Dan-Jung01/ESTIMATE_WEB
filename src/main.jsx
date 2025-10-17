cf4f0614355ad02ad2e94bc158620936800f9d28
Dan <daehan.jung01@gmail.com>
2025-10-18 05:39:13 +0900

    hash

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,4 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-createRoot(document.getElementById('root')).render(<BrowserRouter basename="/ESTIMATE_WEB"><App/></BrowserRouter>);
+createRoot(document.getElementById('root')).render(<HashRouter basename="/ESTIMATE_WEB"><App/></HashRouter>);
