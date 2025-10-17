9d4908765b48f289a1cb907897b39357cd2daa21
Dan <daehan.jung01@gmail.com>
2025-10-18 06:30:22 +0900

    api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
 import axios from 'axios';
+const API_URL = "http://112.163.172.167:4000";
 // export const api = axios.create({ baseURL: 'http://localhost:4000/api', withCredentials: true });
-export const api = axios.create({ baseURL: 'http://192.168.0.50:4000/api', withCredentials: true });
+// export const api = axios.create({ baseURL: 'http://192.168.0.50:4000/api', withCredentials: true });
+export const api = axios.create({ baseURL: API_URL, withCredentials: true });
