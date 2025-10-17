9c31d7fd43bd1cb15624e4481ba9076e4c442eca
Dan <daehan.jung01@gmail.com>
2025-10-18 06:56:04 +0900

    .

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = "http://112.163.172.167:4000";
+const API_URL = "https://onwardly-unsweating-tammy.ngrok-free.dev";
 // export const api = axios.create({ baseURL: 'http://localhost:4000/api', withCredentials: true });
 // export const api = axios.create({ baseURL: 'http://192.168.0.50:4000/api', withCredentials: true });
 export const api = axios.create({ baseURL: API_URL, withCredentials: true });
