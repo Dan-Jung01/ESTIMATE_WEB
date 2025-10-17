0840bb030631cae38bc8ac3f57463ec9c50e4ea9
Dan <daehan.jung01@gmail.com>
2025-10-18 04:34:39 +0900

    gh-pages

--- src/api.js
+++ src/api.js
@@ -1,2 +1,3 @@
 import axios from 'axios';
-export const api = axios.create({ baseURL: 'http://localhost:4000/api', withCredentials: true });
+// export const api = axios.create({ baseURL: 'http://localhost:4000/api', withCredentials: true });
+export const api = axios.create({ baseURL: 'http://192.168.0.50:4000/api', withCredentials: true });
